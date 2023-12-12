a596dee817ecf22be4c7c8f944c59cfb938cff08
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-12 17:30:36 +0700

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,53 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnd',
+    'airbnb-typescript',
+    'plugin:import/typescript'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.ts', '.tsx', '.js', '.jsx']},
+    ],
+    'no-use-before-define': ['error', {variables: false}],
+    'react/prop-types': [
+      'error',
+      {ignore: ['navigation', 'navigation.navigate']},
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
